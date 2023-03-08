a4aa44b5f7912a49e101cdb88932b1c4f9ee991b
tnguyen <ngtrannguyen@gmail.com>
2023-03-08 11:36:39 -0800

    full project

--- src/Droid.java
+++ src/Droid.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Droid {
+/*
+Build a basic droid
+ */
+
+public class Droid {
+
+    int batteryLevel = 100;
+    String name;
+
+    public Droid(String droidName){
+        name = droidName;
+    }
+
+    public String toString(){
+        return "Hello, I’m the droid: " + name;
+    }
+
+    public void performTask(String task){
+        System.out.println(name + " is performing " + task);
+        batteryLevel = batteryLevel -10;
+    }
+
+    public void energyReport(){
+        System.out.println("The battery level is currently: " + batteryLevel);
+    }
+
+    public static void main(String[] args) {
+        Droid myDroid = new Droid("Codey");
+
+        System.out.println(myDroid.toString());
+
+        myDroid.performTask("dancing");
+
+        myDroid.energyReport();
+
+
+
+    }
 }
